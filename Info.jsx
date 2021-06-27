0115420e940076a055c469f623813669e0e79b35
Seetha5 <86562883+Seetha5@users.noreply.github.com>
2021-06-27 20:22:59 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <header>
+        <h1> BESTIES </h1>
+        <p>
+          “Friendship is the hardest thing in the world to explain. ... “Don't
+          make friends who are comfortable to be with. ... “The most beautiful
+          discovery true friends make is that they can grow separately without
+          growing apart.”{" "}
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Info;
